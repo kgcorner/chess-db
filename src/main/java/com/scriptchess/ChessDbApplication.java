b099aff29199794561b21bcde79fc2c2e1f67f67
--get <kumargauravgupta3@gmail.com>
2022-09-16 21:17:21 +0530

    Adds spring boot configs

---
+++ src/main/java/com/scriptchess/ChessDbApplication.java
@@ -0,0 +1,18 @@
+package com.scriptchess;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description : <Write class Description>
+ * Author: kumar
+ * Created on : 16/09/22
+ */
+
+@SpringBootApplication
+public class ChessDbApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChessDbApplication.class, args);
+    }
+}
