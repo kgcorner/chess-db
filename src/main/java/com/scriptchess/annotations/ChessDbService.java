2ecddeaa783373a39c1357613adfa626aafd4555
--get <kumargauravgupta3@gmail.com>
2022-09-18 19:06:48 +0530

    Adds a new annotation to decorate internal classes of chess-db for DI

--- src/main/java/com/scriptchess/annotations/ChessDbService.java
+++ src/main/java/com/scriptchess/annotations/ChessDbService.java
@@ -1,11 +1,14 @@
 package com.scriptchess.annotations;
 
 
+import org.springframework.stereotype.Component;
+
 /**
  * Description : Use this annotation to create instance at run time for dependency injection
  * Author: kumar
  * Created on : 14/09/22
  */
 
+@Component
 public @interface ChessDbService {
 }
