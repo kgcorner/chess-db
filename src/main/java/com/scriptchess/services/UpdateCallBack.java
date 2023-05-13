7c3be1cb0130d6492dec716a10d10f0cbffd5d5c
--get <kumargauravgupta3@gmail.com>
2023-05-14 01:11:26 +0530

    Implements game storage

---
+++ src/main/java/com/scriptchess/services/UpdateCallBack.java
@@ -0,0 +1,11 @@
+package com.scriptchess.services;
+
+/**
+ * Description : <Write class Description>
+ * Author: kumar
+ * Created on : 20/11/22
+ */
+
+public interface UpdateCallBack {
+    void updated(Object object);
+}
